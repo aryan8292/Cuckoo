bb27231465443813ac839c3ceacf92963cd3363c
aryan8292 <94910943+aryan8292@users.noreply.github.com>
2025-06-10 03:51:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -47,7 +47,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'aryan')
 auth_channel = environ.get('AUTH_CHANNEL', "-1002524110318")
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 # Set to False inside the bracket if you don't want to use Request Channel else set it to Channel ID
-REQ_CHANNEL = environ.get("REQ_CHANNEL", "False")
+REQ_CHANNEL = environ.get("REQ_CHANNEL", "")
 REQ_CHANNEL = int(REQ_CHANNEL) if REQ_CHANNEL and id_pattern.search(REQ_CHANNEL) else False
 JOIN_REQS_DB = environ.get("JOIN_REQS_DB", DATABASE_URI)
 
