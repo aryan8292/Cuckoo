4125e45a2fa4b7248457710a4662f5582dcfa239
aryan8292 <94910943+aryan8292@users.noreply.github.com>
2025-06-10 03:41:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -44,7 +44,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'aryan')
 
 # FSUB
-auth_channel = environ.get('AUTH_CHANNEL', "-1002866567891")
+auth_channel = environ.get('AUTH_CHANNEL', "False")
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 # Set to False inside the bracket if you don't want to use Request Channel else set it to Channel ID
 REQ_CHANNEL = environ.get("REQ_CHANNEL", "-1002866567891")
